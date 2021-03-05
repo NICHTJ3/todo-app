629b7fbf76ab993ea154e55befc6a83ee1fa11b1
Trent Nicholson <nicholsontrent@gmail.com>
2021-03-05 16:05:47 +1300

    Style add todo form

--- packages/client/src/components/AddTodo.tsx
+++ packages/client/src/components/AddTodo.tsx
@@ -11,31 +11,43 @@ const AddTodo = ({ saveTodo }: Props) => {
   const [name, setName] = useState("");
   const [description, setDescription] = useState("");
 
+  // NOTE: This should look okay once we're in a container
   return (
-    <form className="Form" onSubmit={(e) => saveTodo(e, { name, description })}>
-      <div>
-        <div>
-          <label htmlFor="name">
-            Name
-            <input
-              onChange={(e) => setName(e.target.value)}
-              type="text"
-              id="name"
-            />
-          </label>
-        </div>
-        <div>
-          <label htmlFor="description">
-            Description
-            <input
-              onChange={(e) => setDescription(e.target.value)}
-              type="text"
-              id="description"
-            />
-          </label>
-        </div>
-      </div>
-      <button type="submit" disabled={!name && !description}>
+    <form
+      className="flex flex-row w-full justify-content-center space-x-2"
+      onSubmit={(e) => {
+        saveTodo(e, { name, description });
+        setName("");
+        setDescription("");
+      }}
+    >
+      <label htmlFor="name" className="sr-only">
+        Name
+      </label>
+      <input
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        placeholder="Name"
+        type="text"
+        id="name"
+        className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+      />
+      <label htmlFor="description" className="sr-only">
+        Description
+      </label>
+      <input
+        value={description}
+        onChange={(e) => setDescription(e.target.value)}
+        placeholder="Description"
+        type="text"
+        id="description"
+        className="appearance-none rounded-md relative block w-full px-3 py-2 border border-gray-300 placeholder-gray-500 text-gray-900 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 focus:z-10 sm:text-sm"
+      />
+      <button
+        type="submit"
+        className="group relative whitespace-nowrap flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
+        disabled={Boolean(!name || !description)}
+      >
         Add Todo
       </button>
     </form>
